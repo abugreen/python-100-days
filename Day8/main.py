bd340d9bb691d5437a83dba8fc16040ffe77c713
abugreen <abugreen0121@gmail.com>
2025-07-21 22:28:20 +0800

    #v01

--- Day8/main.py
+++ Day8/main.py
@@ -1,11 +1,11 @@
+import art
 
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 
     # new_text =""
@@ -43,10 +43,27 @@ def caesar (original_text, shift_mount, encode_or_decode):
     if encode_or_decode == "decode":
         shift_mount *= -1
     for letter in original_text:
-        shifted_pos = alphabet.index(letter)+shift_mount
-        shifted_pos %= len(alphabet) #0-25
-        output_text += alphabet[shifted_pos]  
-    print(f"Here is encode result : {output_text}")    
+        if letter.isalpha():
+            shifted_pos = alphabet.index(letter)+shift_mount
+            shifted_pos %= len(alphabet) #0-25
+            output_text += alphabet[shifted_pos] 
+        else :
+             output_text += letter
+    print(f"Here is {encode_or_decode} : {output_text}")    
 
-caesar(text,shift,direction)
+# caesar(text,shift,direction)
+
+run = True
+while run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text,shift,direction)
+
+    choice = input("Do you want to run this program again?\nType 'yes' or 'no': ")
+    if choice == "no":
+        run = False
+        print("Good Bye")
+
     
