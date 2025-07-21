27e04abfdb6d7613e1705668b9ae2da48143b099
abugreen <abugreen0121@gmail.com>
2025-07-21 22:03:41 +0800

    #

---
+++ Day8/main.py
@@ -0,0 +1,52 @@
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+    # new_text =""
+    # if direction == "encode":
+    #     for i in range(0, len(text)):
+    #         pos = alphabet.index(text[i])
+    #         new_text += alphabet[pos+shift]
+    #     print(new_text)
+    # if direction == "decode":
+    #     for i in range(0, len(text)):
+    #         pos = alphabet.index(text[i])
+    #         new_text += alphabet[pos-shift]
+    #     print(new_text)
+
+# hello ,2
+# def encrypt(original_text , shift_mount):
+#     cipher_text =""
+#     for letter in original_text:
+#         shifted_pos = alphabet.index(letter)+shift_mount
+#         shifted_pos %= len(alphabet) #0-25
+#         cipher_text += alphabet[shifted_pos]  
+#     print(f"Here is encode result : {cipher_text}")
+    
+
+# def dencrypt(original_text , shift_mount):
+#     decode_text = ""
+#     for letter in original_text:
+#         shifted_pos = alphabet.index(letter)-shift_mount
+#         shifted_pos %= len(alphabet)
+#         decide_text += alphabet[shifted_pos]
+#     print(f"Here is dencode result : {dencrypt_text}")
+    
+def caesar (original_text, shift_mount, encode_or_decode):
+    output_text =""
+    if encode_or_decode == "decode":
+        shift_mount *= -1
+    for letter in original_text:
+        shifted_pos = alphabet.index(letter)+shift_mount
+        shifted_pos %= len(alphabet) #0-25
+        output_text += alphabet[shifted_pos]  
+    print(f"Here is encode result : {output_text}")    
+
+caesar(text,shift,direction)
+    
